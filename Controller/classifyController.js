9fb4fe23c7e833ffab65eb942aa9d88989d65d60
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-02-04 21:35:48 +0100

    updated controller

--- Controller/classifyController.js
+++ Controller/classifyController.js
@@ -1,47 +1,42 @@
-const {checkprime,checkperfect,isAmstrong,checkOdd,digitSum, checkEven,funFact} = require('../Utils/functions')
+const { checkprime, checkperfect, isAmstrong, checkOdd, digitSum, checkEven, funFact } = require('../Utils/functions');
 
-const classify = async(req,res)=>{
-    
+const classify = async (req, res) => {
     const number = parseInt(req.query.number);
 
     // Validate input: Check if 'number' is a valid integer
-    if (!/^-?\d+$/.test(number)) {
-        return res.status(400).json({ 
+    if (isNaN(number)) {
+        return res.status(400).json({
             number: "alphabet",
-            error:true
-         });
+            error: true
+        });
     }
 
-    const [check_prime,check_perfect,check_armstrong,check_odd,check_even,sum_of_digits,fun_fact] = 
-    await Promise.all([checkprime(number), checkperfect(number),isAmstrong(number),checkOdd(number),checkEven(number),digitSum(number),funFact(number)]);
+    // Run calculations in parallel for better performance
+    const results = await Promise.all([
+        checkprime(number),
+        checkperfect(number),
+        isAmstrong(number),
+        funFact(number)
+    ]);
 
-    
-    // const check_prime = checkprime(number);
-    // const check_perfect = checkperfect(number);
-    // const check_armstrong = isAmstrong(number);
-    // const check_odd = checkOdd(number);
-    // const check_even = checkEven(number);
-    // const sum_of_digits = digitSum(number);
+    // Get synchronous results immediately
+    const check_odd = checkOdd(number);
+    const check_even = checkEven(number);
+    const sum_of_digits = digitSum(number);
 
     const properties = [];
-    if(check_armstrong) properties.push("armstrong") 
-    if(check_odd) properties.push("odd")
-    if(check_even) properties.push("even")
-
-
-
+    if (results[2]) properties.push("armstrong");
+    if (check_odd) properties.push("odd");
+    if (check_even) properties.push("even");
 
     return res.status(200).json({
-            number,
-            "is_prime": check_prime,
-            "is_perfect": check_perfect,
-            "properties": properties,
-            "digit_sum": sum_of_digits,
-            "fun_fact": fun_fact
-
-       
+        number,
+        "is_prime": results[0],
+        "is_perfect": results[1],
+        "properties": properties,
+        "digit_sum": sum_of_digits,
+        "fun_fact": results[3]
     });
+};
 
-
-}
 module.exports = classify;
--- Utils/functions.js
+++ Utils/functions.js
@@ -1,102 +1,74 @@
 const axios = require('axios');
 
-function checkEven(number){
-    return (number%2 === 0)
-
+function checkEven(number) {
+    return number % 2 === 0;
 }
 
-function checkOdd(number){
-return (number%2 !== 0);
-
+function checkOdd(number) {
+    return number % 2 !== 0;
 }
 
-function checkprime(number){
-    if(number<2) return false ;
-    if(number === 2) return true;
-    if(number% 2 === 0) return false;
+// Optimized Prime Check
+function checkprime(number) {
+    if (number < 2) return false;
+    if (number === 2 || number === 3) return true;
+    if (number % 2 === 0) return false;
 
-
-    for(i=3; i<Math.sqrt(number); i+=2){
-        if(number%i === 0) return false;
-
+    for (let i = 3; i <= Math.sqrt(number) + 1; i += 2) {
+        if (number % i === 0) return false;
     }
-
-    return true
-
+    return true;
 }
 
-function checkperfect(number){
-    if (number<2) return false;
-
-    let sum=1;
-
-    for(let i=2; i <Math.sqrt(number); i++){
-
-        if (number %i === 0){
-
-           sum += i;
-           if(i !== number/i){
-            sum+=number/i;
-           }
-         }
-     
-     }
-        
-    return sum == number;
-
+// Optimized Perfect Number Check
+function checkperfect(number) {
+    if (number < 2) return false;
+    
+    let sum = 1;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            sum += i + (i !== number / i ? number / i : 0);
+        }
+    }
+    
+    return sum === number;
 }
 
-function isAmstrong(number){
-    // let sum = 0;
-    // let orgnum = number ;
-    // while( number!== 0){
-    //     let cubeOfdigit = Math.pow((number %10),3)
-    //     sum += cubeOfdigit;
-    //     number = Math.floor(number/10);
-    // }
-    const digits = number.toString().split('');
-
+// Optimized Armstrong Number Check
+function isAmstrong(number) {
+    const digits = number.toString();
     const numDigits = digits.length;
-
-    const sum = digits.reduce((acc,digit)=>{
-
-        return acc+ Math.pow(Number(digit),numDigits);
-        
-        },0);
+    const sum = [...digits].reduce((acc, digit) => acc + Math.pow(Number(digit), numDigits), 0);
 
     return sum === number;
-
 }
-function digitSum(number){
+
+// Optimized Digit Sum
+function digitSum(number) {
     let sum = 0;
-    while( number !== 0){
-        sum += number%10;
-        number= Math.floor(number/10);
+    while (number !== 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
     }
-    return sum
-
+    return sum;
 }
+
+// Caching for Number Fun Fact
 const cache = new Map();
 
-
 async function funFact(number) {
-    if(cache.has(number)){
+    if (cache.has(number)) {
         return cache.get(number);
     }
 
     try {
-
-        const response = await axios.get(`http://numbersapi.com/${number}/math?json`);
-        console.log(response);
-
-        const fact = await response.data.text
-        cache.set(number,fact);
-
-        return fact; 
-
+        const { data } = await axios.get(`http://numbersapi.com/${number}/math?json`);
+        cache.set(number, data.text);
+        return data.text;
     } catch (error) {
         console.error("Error fetching number fact:", error);
         return "Could not retrieve fact";
     }
 }
-module.exports ={funFact,digitSum,isAmstrong,checkperfect,checkprime, checkEven,checkOdd,}
+
+module.exports = { funFact, digitSum, isAmstrong, checkperfect, checkprime, checkEven, checkOdd };
