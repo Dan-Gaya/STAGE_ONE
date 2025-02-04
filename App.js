552510fe96b4606e320c0440a171fed922729221
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-02-04 13:26:04 +0100

    vercel json was added and server.js was also updated for deployement

--- App.js
+++ App.js
@@ -17,4 +17,11 @@ app.use('/',(req,res,next)=>{
     res.send('welcome to HNG12 task 1')
 })
 
+app.use((error,req,res,next) =>{
+    res.status(500).json({
+        message: 'Server in the main app failed to start',
+        error: error.message,
+    });
+});
+
 module.exports = app;
