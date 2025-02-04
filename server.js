566cdefb26eed569ec367223d886f0fe39c19346
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-02-04 13:05:02 +0100

    vercel json was added and server.js was also updated for deployement

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT,()=>{
-    console.log(`local server started at http://localhost:${PORT}`)
+// server.listen(PORT,()=>{
+//     console.log(`local server started at http://localhost:${PORT}`)
 
-});
+// });
